57a5a833809f24be449bda04a1adb47d1cbfbf43
bariryan <bariryan@gmail.com>
2022-06-16 09:28:46 +0000

    added function layout

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnser(){
+
+}
+
+function incrementScore(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+    
+}
