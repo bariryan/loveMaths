502aa1d36557050a3fc4636c05819f8d8260bc5e
bariryan <bariryan@gmail.com>
2022-06-16 09:56:21 +0000

    added event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button"); 
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if(this.getAttribute("data-type")=== "submit"){
+               alert("You clicked Submit!");
+            } else{
+               let gameType = this.getAttribute("data-type");
+               alert(`you clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGame(){
 
 }
@@ -23,5 +37,5 @@ function displaySubtractQuestion(){
 }
 
 function displayMultiplyQuestion(){
-    
+
 }
